e14564f82f7804aa6419737c9ba2892cf775b35a
Ben Elferink <ben.elferink@icloud.com>
2025-01-28 12:46:38 +0200

    fix: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import json from '@rollup/plugin-json'
 import typescript from 'rollup-plugin-typescript2'
 import postcss from 'rollup-plugin-postcss'
 
@@ -21,14 +19,5 @@ export default {
       sourcemap: true,
     },
   ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    json(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    postcss({
-      extensions: ['.css'],
-    }),
-  ],
+  plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss({ extensions: ['.css'] })],
 }
