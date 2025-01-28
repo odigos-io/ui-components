c533cc033b062d8264b82e9c79c38c0a2a17ef6f
Ben Elferink <ben.elferink@icloud.com>
2025-01-28 12:50:04 +0200

    fix: export using index

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * as types from './@types'
 export * as theme from './styles'
 export * as icons from './icons'
 
-export * from './components/button'
+// Components should point at the index files, to exclude Storybook stories
+export * from './components/button/index'
