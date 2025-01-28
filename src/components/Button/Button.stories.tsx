fa200b8d24dba90cf359ffa75721675549192c38
Ben Elferink <ben.elferink@icloud.com>
2025-01-28 12:14:30 +0200

    feat: add actual button component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,13 +1,36 @@
-import { StoryObj } from '@storybook/react'
-import { Button } from './Button'
+import React, { useEffect } from 'react'
+import { getTheme } from '../../styles'
+import { ThemeProvider } from 'styled-components'
+import { type StoryFn, type StoryObj } from '@storybook/react'
+import { Button, type ButtonProps } from '.'
+
+interface Props extends ButtonProps {
+  darkMode: boolean
+}
 
 export default {
   title: 'Components/Button',
   component: Button,
 }
 
-export const Default: StoryObj<typeof Button> = {
-  args: {
-    children: 'Button',
-  },
+// Create a master template for mapping props to render the Button component
+const Template: StoryFn<Props> = ({ darkMode, ...props }) => {
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? '#000' : '#fff'
+  }, [darkMode])
+
+  return (
+    <ThemeProvider theme={getTheme(darkMode)}>
+      <Button {...props} />
+    </ThemeProvider>
+  )
+}
+
+// Reuse that template for creating different stories
+export const Default: StoryObj<Props> = Template.bind({})
+
+Default.args = {
+  darkMode: true,
+  variant: 'primary',
+  children: 'click me',
 }
