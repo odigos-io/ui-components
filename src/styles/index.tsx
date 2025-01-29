f562024da71535065d2bdc0823fce83e8705930d
Ben Elferink <ben.elferink@icloud.com>
2025-01-29 11:59:16 +0200

    feat: add globals.css

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -1,11 +1,12 @@
 import React, { type FC, type PropsWithChildren } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { getTheme, type ITheme } from './theme'
-
-export * from './animations'
-export * from './opacity'
-export { type ITheme }
+import './globals.css'
 
 export const Provider: FC<PropsWithChildren<{ darkMode: boolean }>> = ({ children, darkMode }) => {
   return <ThemeProvider theme={getTheme(darkMode) as ITheme}>{children}</ThemeProvider>
 }
+
+export * from './animations'
+export * from './opacity'
+export { type ITheme }
