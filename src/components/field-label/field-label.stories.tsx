276af3b9b527fdbfc49f65adca17c87f96cb65d1
Ben Elferink <ben.elferink@icloud.com>
2025-01-29 13:46:21 +0200

    feat: add field-label

---
+++ src/components/field-label/field-label.stories.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import { Theme } from '../..'
+import { FieldLabel, type FieldLabelProps } from '.'
+import { type StoryFn, type StoryObj } from '@storybook/react'
+
+interface Props extends FieldLabelProps {
+  darkMode: boolean
+}
+
+export default {
+  title: 'Components/FieldLabel',
+  component: FieldLabel,
+}
+
+// Create a master template for mapping props to render the Button component
+const Template: StoryFn<Props> = ({ darkMode, ...props }) => {
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? '#000' : '#fff'
+  }, [darkMode])
+
+  return (
+    <Theme.Provider darkMode={darkMode}>
+      <FieldLabel {...props} />
+    </Theme.Provider>
+  )
+}
+
+// Reuse that template for creating different stories
+export const Default: StoryObj<Props> = Template.bind({})
+
+Default.args = {
+  darkMode: true,
+  title: 'First Name',
+  tooltip: 'The name given to you at birth',
+  required: false,
+}
---
+++ src/components/field-label/index.tsx
@@ -0,0 +1,48 @@
+import React, { forwardRef, type CSSProperties } from 'react'
+import { Text } from '../text'
+import { Tooltip } from '../tooltip'
+import styled from 'styled-components'
+
+interface FieldLabelProps {
+  title?: string
+  required?: boolean
+  tooltip?: string
+  style?: CSSProperties
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  margin-bottom: 4px;
+`
+
+const Title = styled(Text)`
+  font-size: 14px;
+  opacity: 0.8;
+  line-height: 22px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`
+
+const OptionalText = styled(Text)`
+  font-size: 12px;
+  color: ${({ theme }) => theme.text.darker_grey};
+  opacity: 0.8;
+`
+
+const FieldLabel = forwardRef<HTMLDivElement, FieldLabelProps>(({ title, required, tooltip, style }, ref) => {
+  if (!title) return null
+
+  return (
+    <Wrapper ref={ref} style={style}>
+      <Tooltip text={tooltip} withIcon>
+        <Title>{title}</Title>
+        {!required && <OptionalText>(optional)</OptionalText>}
+      </Tooltip>
+    </Wrapper>
+  )
+})
+
+export { FieldLabel, type FieldLabelProps }
