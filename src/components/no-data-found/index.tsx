04b2ca4db30c4a7bd0d3ccac28f36a3f217e06ec
Ben Elferink <ben.elferink@icloud.com>
2025-01-29 14:45:03 +0200

    feat: add no-data-found

---
+++ src/components/no-data-found/index.tsx
@@ -0,0 +1,51 @@
+import React, { forwardRef } from 'react'
+import { Text } from '../text'
+import { NoDataIcon } from '../../icons'
+import styled, { useTheme } from 'styled-components'
+
+interface NoDataFoundProps {
+  title?: string
+  subTitle?: string
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`
+
+const Title = styled(Text)`
+  color: ${({ theme }) => theme.text.darker_grey};
+  line-height: 24px;
+`
+
+const SubTitle = styled(Text)`
+  color: ${({ theme }) => theme.colors.border};
+  font-size: 14px;
+  font-weight: 200;
+  line-height: 20px;
+`
+
+const NoDataFound = forwardRef<HTMLDivElement, NoDataFoundProps>(
+  ({ title = 'No data found', subTitle = 'Check your search phrase and try one more time' }, ref) => {
+    const theme = useTheme()
+
+    return (
+      <Container ref={ref}>
+        <TitleWrapper>
+          <NoDataIcon fill={theme.text.dark_grey} />
+          <Title>{title}</Title>
+        </TitleWrapper>
+        {subTitle && <SubTitle>{subTitle}</SubTitle>}
+      </Container>
+    )
+  }
+)
+
+export { NoDataFound, type NoDataFoundProps }
---
+++ src/components/no-data-found/no-data-found.stories.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react'
+import { Theme } from '../..'
+import { NoDataFound, type NoDataFoundProps } from '.'
+import { type StoryFn, type StoryObj } from '@storybook/react'
+
+interface Props extends NoDataFoundProps {
+  darkMode: boolean
+}
+
+export default {
+  title: 'Components/NoDataFound',
+  component: NoDataFound,
+}
+
+// Create a master template for mapping props to render the Button component
+const Template: StoryFn<Props> = ({ darkMode, ...props }) => {
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? '#000' : '#fff'
+  }, [darkMode])
+
+  return (
+    <Theme.Provider darkMode={darkMode}>
+      <NoDataFound {...props} />
+    </Theme.Provider>
+  )
+}
+
+// Reuse that template for creating different stories
+export const Default: StoryObj<Props> = Template.bind({})
+
+Default.args = {
+  darkMode: true,
+}
