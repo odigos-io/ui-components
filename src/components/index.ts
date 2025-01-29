4a2fbd0243a72c9d50f7332d2ecc40f3c235601f
Ben Elferink <ben.elferink@icloud.com>
2025-01-29 11:57:04 +0200

    feat: add segment

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@
 export * from './badge/index'
 export * from './button/index'
 export * from './divider/index'
+export * from './segment/index'
 export * from './text/index'
 export * from './toggle/index'
 export * from './tooltip/index'
---
+++ src/components/segment/segment.stories.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import { CheckCircledIcon, CrossCircledIcon, Theme } from '../..'
+import { Segment, type SegmentProps } from '.'
+import { type StoryFn, type StoryObj } from '@storybook/react'
+
+interface Props extends SegmentProps {
+  darkMode: boolean
+}
+
+export default {
+  title: 'Components/Segment',
+  component: Segment,
+}
+
+// Create a master template for mapping props to render the Button component
+const Template: StoryFn<Props> = ({ darkMode, ...props }) => {
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? '#000' : '#fff'
+  }, [darkMode])
+
+  const [val, setVal] = useState(true)
+
+  return (
+    <Theme.Provider darkMode={darkMode}>
+      <Segment {...props} selected={val} setSelected={setVal} />
+    </Theme.Provider>
+  )
+}
+
+// Reuse that template for creating different stories
+export const Default: StoryObj<Props> = Template.bind({})
+
+Default.args = {
+  darkMode: true,
+  options: [
+    { icon: CheckCircledIcon, label: 'active', value: true, selectedBgColor: 'green' },
+    { icon: CrossCircledIcon, label: 'inactive', value: false, selectedBgColor: 'red' },
+  ],
+}
