6301be3740bf7656adf74d4926a868edc7148de7
Ben Elferink <ben.elferink@icloud.com>
2025-01-29 13:46:05 +0200

    feat: add field-error

---
+++ src/components/field-error/field-error.stories.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { Theme } from '../..'
+import { FieldError, type FieldErrorProps } from '.'
+import { type StoryFn, type StoryObj } from '@storybook/react'
+
+interface Props extends FieldErrorProps {
+  darkMode: boolean
+}
+
+export default {
+  title: 'Components/FieldError',
+  component: FieldError,
+}
+
+// Create a master template for mapping props to render the Button component
+const Template: StoryFn<Props> = ({ darkMode, ...props }) => {
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? '#000' : '#fff'
+  }, [darkMode])
+
+  return (
+    <Theme.Provider darkMode={darkMode}>
+      <FieldError {...props} />
+    </Theme.Provider>
+  )
+}
+
+// Reuse that template for creating different stories
+export const Default: StoryObj<Props> = Template.bind({})
+
+Default.args = {
+  darkMode: true,
+  children: 'This field is required!',
+}
---
+++ src/components/field-error/index.tsx
@@ -0,0 +1,24 @@
+import React, { forwardRef, type PropsWithChildren } from 'react'
+import { Text } from '../text'
+import styled from 'styled-components'
+
+interface FieldErrorProps extends PropsWithChildren {}
+
+const ErrorWrapper = styled.div`
+  padding: 4px 0 0 0;
+`
+
+const ErrorMessage = styled(Text)`
+  font-size: 12px;
+  color: ${({ theme }) => theme.text.error};
+`
+
+const FieldError = forwardRef<HTMLDivElement, FieldErrorProps>(({ children }) => {
+  return (
+    <ErrorWrapper>
+      <ErrorMessage>{children}</ErrorMessage>
+    </ErrorWrapper>
+  )
+})
+
+export { FieldError, type FieldErrorProps }
