869bc26bc6784c3064aea8d59b9469de173ce7de
Ben Elferink <ben.elferink@icloud.com>
2025-01-29 15:53:02 +0200

    feat: add extend-arrow

---
+++ src/components/extend-arrow/extend-arrow.stories.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import { Theme } from '../..'
+import { ExtendArrow, type ExtendArrowProps } from '.'
+import { type StoryFn, type StoryObj } from '@storybook/react'
+
+interface Props extends ExtendArrowProps {
+  darkMode: boolean
+}
+
+export default {
+  title: 'Components/ExtendArrow',
+  component: ExtendArrow,
+}
+
+// Create a master template for mapping props to render the Button component
+const Template: StoryFn<Props> = ({ darkMode, ...props }) => {
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? '#000' : '#fff'
+  }, [darkMode])
+
+  return (
+    <Theme.Provider darkMode={darkMode}>
+      <ExtendArrow {...props} />
+    </Theme.Provider>
+  )
+}
+
+// Reuse that template for creating different stories
+export const Default: StoryObj<Props> = Template.bind({})
+
+Default.args = {
+  darkMode: true,
+  extend: false,
+  size: 69,
+}
---
+++ src/components/extend-arrow/index.tsx
@@ -0,0 +1,23 @@
+import React, { forwardRef } from 'react'
+import styled from 'styled-components'
+import { ExtendArrowIcon } from '../../icons'
+
+interface ExtendArrowProps {
+  extend: boolean
+  size?: number
+  align?: 'left' | 'right' | 'center'
+}
+
+const Container = styled.div<{ $align?: ExtendArrowProps['align'] }>`
+  margin: ${({ $align }) => ($align === 'right' ? 'auto 0 auto auto' : $align === 'left' ? 'auto auto auto 0' : 'auto')};
+`
+
+const ExtendArrow = forwardRef<HTMLDivElement, ExtendArrowProps>(({ extend, size = 14, align = 'center' }) => {
+  return (
+    <Container $align={align}>
+      <ExtendArrowIcon size={size} rotate={extend ? 180 : 0} />
+    </Container>
+  )
+})
+
+export { ExtendArrow, type ExtendArrowProps }
