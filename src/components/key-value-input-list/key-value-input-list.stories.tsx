28dd2db55f28713c499dff2b01a19bb4cfca6053
Ben Elferink <ben.elferink@icloud.com>
2025-01-30 11:37:42 +0200

    feat: add key-value-input-list

---
+++ src/components/key-value-input-list/key-value-input-list.stories.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { Theme } from '../..'
+import { KeyValueInputsList, type KeyValueInputsListProps } from '.'
+import { type StoryFn, type StoryObj } from '@storybook/react'
+
+interface Props extends KeyValueInputsListProps {
+  darkMode: boolean
+}
+
+export default {
+  title: 'Components/KeyValueInputsList',
+  component: KeyValueInputsList,
+}
+
+// Create a master template for mapping props to render the Button component
+const Template: StoryFn<Props> = ({ darkMode, ...props }) => {
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? '#000' : '#fff'
+  }, [darkMode])
+
+  return (
+    <Theme.Provider darkMode={darkMode}>
+      <KeyValueInputsList {...props} />
+    </Theme.Provider>
+  )
+}
+
+// Reuse that template for creating different stories
+export const Default: StoryObj<Props> = Template.bind({})
+
+Default.args = {
+  darkMode: true,
+  title: 'Request Headers',
+}
