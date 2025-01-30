5c41579e2450ac8780673d6144081ea0ffc094c0
Ben Elferink <ben.elferink@icloud.com>
2025-01-30 11:21:49 +0200

    feat: add input-list

---
+++ src/components/input-list/input-list.stories.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { Theme } from '../..'
+import { InputList, type InputListProps } from '.'
+import { type StoryFn, type StoryObj } from '@storybook/react'
+
+interface Props extends InputListProps {
+  darkMode: boolean
+}
+
+export default {
+  title: 'Components/InputList',
+  component: InputList,
+}
+
+// Create a master template for mapping props to render the Button component
+const Template: StoryFn<Props> = ({ darkMode, ...props }) => {
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? '#000' : '#fff'
+  }, [darkMode])
+
+  return (
+    <Theme.Provider darkMode={darkMode}>
+      <InputList {...props} />
+    </Theme.Provider>
+  )
+}
+
+// Reuse that template for creating different stories
+export const Default: StoryObj<Props> = Template.bind({})
+
+Default.args = {
+  darkMode: true,
+  title: 'Pet Names 🐶🐱',
+}
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,1 +1,2 @@
 export * from './get-status-icon'
+export * from './is-emtpy'
