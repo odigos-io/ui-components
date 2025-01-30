8ee8d931408c7f45cd3e930415a80c1b253cf6c2
Ben Elferink <ben.elferink@icloud.com>
2025-01-30 11:52:29 +0200

    feat: add dropdown

---
+++ src/components/dropdown/dropdown.stories.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+import { Theme } from '../..'
+import { Dropdown, type DropdownProps } from '.'
+import { type StoryFn, type StoryObj } from '@storybook/react'
+
+interface Props extends DropdownProps {
+  darkMode: boolean
+}
+
+export default {
+  title: 'Components/Dropdown',
+  component: Dropdown,
+}
+
+// Create a master template for mapping props to render the component
+const Template: StoryFn<Props> = ({ darkMode, ...props }) => {
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? '#000' : '#fff'
+  }, [darkMode])
+
+  return (
+    <Theme.Provider darkMode={darkMode}>
+      <Dropdown {...props} />
+    </Theme.Provider>
+  )
+}
+
+// Reuse that template for creating different stories
+export const Default: StoryObj<Props> = Template.bind({})
+
+Default.args = {
+  darkMode: true,
+  title: 'Gender',
+  options: [
+    {
+      id: 'male',
+      value: 'Male',
+    },
+    {
+      id: 'female',
+      value: 'Female',
+    },
+  ],
+}
