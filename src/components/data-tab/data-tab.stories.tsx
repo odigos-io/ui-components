8744e79ed28a47272b6ef3a1d922d46c289d176e
Ben Elferink <ben.elferink@icloud.com>
2025-02-02 09:49:25 +0200

    feat: add data-tab

---
+++ src/components/data-tab/data-tab.stories.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import { Checkbox, ErrorTriangleIcon, getProgrammingLanguageIcon, Theme } from '../..'
+import { DataTab, type DataTabProps } from '.'
+import { type StoryFn, type StoryObj } from '@storybook/react'
+import { PROGRAMMING_LANGUAGES } from '../../@types'
+
+interface Props extends DataTabProps {
+  darkMode: boolean
+}
+
+export default {
+  title: 'Components/DataTab',
+  component: DataTab,
+}
+
+// Create a master template for mapping props to render
+const Template: StoryFn<Props> = ({ darkMode, ...props }) => {
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? '#000' : '#fff'
+  }, [darkMode])
+
+  return (
+    <Theme.Provider darkMode={darkMode}>
+      <div style={{ width: '270px' }}>
+        <DataTab {...props} />
+      </div>
+    </Theme.Provider>
+  )
+}
+
+// Reuse that template for creating different stories
+export const Default: StoryObj<Props> = Template.bind({})
+
+Default.args = {
+  darkMode: true,
+  title: 'node-auth-service-v69',
+  subTitle: 'default • deployment',
+  iconSrc: getProgrammingLanguageIcon(PROGRAMMING_LANGUAGES.JAVASCRIPT),
+  isError: true,
+  renderActions: () => (
+    <>
+      <ErrorTriangleIcon size={20} />
+      <Checkbox />
+    </>
+  ),
+  onClick: () => {},
+}
---
+++ src/components/data-tab/index.tsx
@@ -0,0 +1,225 @@
+import React, { type FC, Fragment, useCallback, useEffect, useRef, useState } from 'react'
+import { Text } from '../text'
+import { Status } from '../status'
+import { Tooltip } from '../tooltip'
+import { Divider } from '../divider'
+import { hexPercent } from '../../styles'
+import { IconButton } from '../icon-button'
+import { IconWrapped } from '../icon-wrapped'
+import { ExtendArrow } from '../extend-arrow'
+import styled, { css } from 'styled-components'
+import { MonitorsIcons } from '../monitors-icons'
+import { FlexColumn, FlexRow } from '../../styled'
+import { NOTIFICATION_TYPE, SIGNAL_TYPE, type SVG } from '../../@types'
+
+interface DataTabProps {
+  title: string
+  subTitle?: string
+  icon?: SVG
+  iconSrc?: string
+  hoverText?: string
+  monitors?: SIGNAL_TYPE[]
+  monitorsWithLabels?: boolean
+  isActive?: boolean
+  isError?: boolean
+  withExtend?: boolean
+  isExtended?: boolean
+  renderExtended?: () => React.ReactNode
+  renderActions?: () => React.ReactNode
+  onClick?: () => void
+}
+
+const ControlledVisibility = styled.div`
+  visibility: hidden;
+`
+
+const Container = styled.div<{ $withClick: boolean; $isError: DataTabProps['isError'] }>`
+  display: flex;
+  flex-direction: column;
+  align-self: stretch;
+  padding: 16px;
+  width: calc(100% - 32px);
+  border-radius: 16px;
+  background-color: ${({ $isError, theme }) => ($isError ? theme.text.error + hexPercent['010'] : theme.colors.secondary + hexPercent['005'])};
+
+  ${({ $withClick, $isError, theme }) =>
+    $withClick &&
+    css`
+      &:hover {
+        cursor: pointer;
+        background-color: ${$isError ? theme.text.error + hexPercent['020'] : theme.colors.secondary + hexPercent['010']};
+        ${ControlledVisibility} {
+          visibility: visible;
+        }
+      }
+    `}
+
+  &:hover {
+    ${ControlledVisibility} {
+      visibility: visible;
+    }
+  }
+`
+
+const Title = styled(Text)<{ $maxWidth: number }>`
+  max-width: ${({ $maxWidth }) => $maxWidth}px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  font-size: 14px;
+  &::after {
+    // This is to prevent the browser "default tooltip" from appearing when the title is too long
+    content: '';
+    display: block;
+  }
+`
+
+const SubTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`
+
+const SubTitle = styled(Text)`
+  color: ${({ theme }) => theme.text.grey};
+  font-size: 10px;
+`
+
+const ActionsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  margin-left: auto;
+`
+
+const HoverText = styled(Text)`
+  margin-right: 16px;
+`
+
+const DataTab: FC<DataTabProps> = ({
+  title,
+  subTitle,
+  icon,
+  iconSrc,
+  hoverText,
+  monitors,
+  monitorsWithLabels,
+  isActive,
+  isError,
+  withExtend,
+  isExtended,
+  renderExtended,
+  renderActions,
+  onClick,
+  ...props
+}) => {
+  const [extend, setExtend] = useState(isExtended || false)
+  const [maxWidth, setMaxWidth] = useState(0)
+  const [isTitleOverflowed, setIsTitleOverflowed] = useState(false)
+
+  const containerRef = useRef<HTMLDivElement>(null)
+  const actionsRef = useRef<HTMLDivElement>(null)
+  const titleRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (containerRef.current && actionsRef.current) {
+      const containerWidth = containerRef.current.clientWidth
+      const actionsWidth = actionsRef.current.clientWidth
+
+      // 85 is the sum of the container-padding, icon width, and flex-row-gap
+      setMaxWidth(containerWidth - actionsWidth - 85)
+    }
+  }, [])
+
+  useEffect(() => {
+    const { current } = titleRef
+
+    if (current) {
+      const { clientWidth } = current
+      const marginUp = maxWidth * 1.05 // add 5%
+      const marginDown = maxWidth * 0.95 // subtract 5%
+
+      setIsTitleOverflowed(clientWidth < marginUp && clientWidth > marginDown)
+    }
+  }, [title, maxWidth])
+
+  const renderMonitors = useCallback(
+    (withSeperator: boolean) => {
+      if (!monitors || !monitors.length) return null
+
+      return (
+        <>
+          {withSeperator && <SubTitle>{'•'}</SubTitle>}
+          <MonitorsIcons monitors={monitors} withLabels={monitorsWithLabels} size={10} />
+        </>
+      )
+    },
+    [monitors]
+  )
+
+  const renderActiveStatus = useCallback(
+    (withSeperator: boolean) => {
+      if (typeof isActive !== 'boolean') return null
+
+      return (
+        <>
+          {withSeperator && <SubTitle>{'•'}</SubTitle>}
+          <Status status={isActive ? NOTIFICATION_TYPE.SUCCESS : NOTIFICATION_TYPE.ERROR} size={10} />
+        </>
+      )
+    },
+    [isActive]
+  )
+
+  return (
+    <Container ref={containerRef} $isError={isError} $withClick={!!onClick} onClick={onClick} {...props}>
+      <FlexRow $gap={8}>
+        <div>
+          <IconWrapped icon={icon} src={iconSrc} status={isError ? NOTIFICATION_TYPE.ERROR : undefined} />
+        </div>
+
+        <FlexColumn $gap={4}>
+          <Tooltip text={isTitleOverflowed ? title : undefined} withIcon={false}>
+            <Title ref={titleRef} $maxWidth={maxWidth}>
+              {title}
+            </Title>
+          </Tooltip>
+
+          <SubTitleWrapper>
+            {subTitle && <SubTitle>{subTitle}</SubTitle>}
+            {renderMonitors(!!subTitle)}
+            {renderActiveStatus(!!monitors?.length)}
+          </SubTitleWrapper>
+        </FlexColumn>
+
+        <ActionsWrapper ref={actionsRef}>
+          {!!hoverText && (
+            <ControlledVisibility>
+              <HoverText size={14} family='secondary'>
+                {hoverText}
+              </HoverText>
+            </ControlledVisibility>
+          )}
+          {renderActions && renderActions()}
+          {withExtend && (
+            <Fragment>
+              <Divider orientation='vertical' length='16px' margin='0 2px' />
+              <IconButton onClick={() => setExtend((prev) => !prev)}>
+                <ExtendArrow extend={extend} />
+              </IconButton>
+            </Fragment>
+          )}
+        </ActionsWrapper>
+      </FlexRow>
+
+      {extend && renderExtended && (
+        <FlexColumn>
+          <Divider margin='16px 0' />
+          {renderExtended()}
+        </FlexColumn>
+      )}
+    </Container>
+  )
+}
+
+export { DataTab, type DataTabProps }
