81152b7be8bbcccd886113e83e53de768851a957
Ben Elferink <ben.elferink@icloud.com>
2025-02-05 10:12:51 +0200

    feat: add trace-loader

---
+++ src/components/trace-loader/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Lottie from 'react-lottie'
+import styled from 'styled-components'
+import animationData from './lottie.json'
+
+interface TraceLoaderProps {
+  width?: number
+}
+
+const Container = styled.div<{ $width: number; $height: number }>`
+  width: ${({ $width }) => $width}px;
+  height: ${({ $height }) => $height}px;
+  position: relative;
+`
+
+const TraceLoader: React.FC<TraceLoaderProps> = ({ width: w = 620 }) => {
+  const ratio = 620 / 220 // preserve aspect ratio
+  const width = w / ratio
+  const height = w
+
+  return (
+    // Note: The container width and height are swapped because the animation is rotated
+    <Container $width={height} $height={width}>
+      <Lottie
+        width={width}
+        height={height}
+        isClickToPauseDisabled
+        options={{
+          loop: true,
+          autoplay: true,
+          animationData: animationData,
+        }}
+        style={{
+          transform: 'rotate(-90deg)',
+          position: 'absolute',
+          top: -(width - width / 10),
+          left: width - width / 10,
+        }}
+      />
+    </Container>
+  )
+}
+
+export { TraceLoader, type TraceLoaderProps }
---
+++ src/components/trace-loader/trace-loader.stories.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { type StoryFn, type StoryObj } from '@storybook/react'
+import { Theme } from '@odigos/ui-theme'
+import { TraceLoader, type TraceLoaderProps } from '.'
+
+interface Props extends TraceLoaderProps {
+  darkMode: boolean
+}
+
+export default {
+  title: 'Components/TraceLoader',
+  component: TraceLoader,
+}
+
+// Create a master template for mapping props to render
+const Template: StoryFn<Props> = ({ darkMode, ...props }) => {
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? '#000' : '#fff'
+  }, [darkMode])
+
+  return (
+    <Theme.Provider darkMode={darkMode}>
+      <TraceLoader {...props} />
+    </Theme.Provider>
+  )
+}
+
+// Reuse that template for creating different stories
+export const Default: StoryObj<Props> = Template.bind({})
+
+Default.args = {
+  darkMode: true,
+  width: 500,
+}
