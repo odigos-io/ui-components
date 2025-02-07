bd44408a50929695fdafed704fc487fdddf1bede
Ben Elferink <ben.elferink@icloud.com>
2025-02-07 23:03:53 +0200

    feat: badge clickable

--- src/components/badge/badge.stories.tsx
+++ src/components/badge/badge.stories.tsx
@@ -14,4 +14,5 @@ export const Default: StoryFn<BadgeProps> = (props) => {
 Default.args = {
   label: 69,
   filled: true,
+  onClick: () => {},
 }
--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -1,12 +1,15 @@
+import Theme from '@odigos/ui-theme'
 import React, { type ReactNode, type FC } from 'react'
 import styled from 'styled-components'
 
 interface BadgeProps {
   label: string | number | ReactNode
   filled?: boolean
+  onClick?: () => void
 }
 
-const Styled = styled.span<{ $filled: BadgeProps['filled'] }>`
+const Styled = styled.span<{ $filled: BadgeProps['filled']; $clickable: boolean }>`
+  min-height: 16px;
   min-width: 16px;
   width: fit-content;
   padding: 4px;
@@ -14,16 +17,26 @@ const Styled = styled.span<{ $filled: BadgeProps['filled'] }>`
   align-items: center;
   justify-content: center;
   border-radius: 50px;
-  border: 1px solid ${({ theme, $filled }) => ($filled ? theme.colors.majestic_blue : theme.colors.border)};
+  border: 1px solid ${({ theme, $filled }) => ($filled ? 'transparent' : theme.colors.border)};
   background-color: ${({ theme, $filled }) => ($filled ? theme.colors.majestic_blue : 'transparent')};
   color: ${({ theme, $filled }) => ($filled ? theme.text.white : theme.text.grey)};
   font-family: ${({ theme }) => theme.font_family.secondary};
   font-size: 12px;
   white-space: nowrap;
+  ${({ theme, $filled, $clickable }) =>
+    $clickable &&
+    `&:hover {
+      cursor: pointer;
+      background-color: ${$filled ? theme.colors.majestic_blue_soft : theme.colors.secondary + Theme.opacity.hex['020']};
+    }`}
 `
 
-const Badge: FC<BadgeProps> = ({ label, filled }) => {
-  return <Styled $filled={filled}>{label}</Styled>
+const Badge: FC<BadgeProps> = ({ label, filled, onClick }) => {
+  return (
+    <Styled $filled={filled} $clickable={!!onClick} onClick={onClick}>
+      {label}
+    </Styled>
+  )
 }
 
 export { Badge, type BadgeProps }
