71d4fe635ac5e1fdb2e87db2707ca11540a5a714
Ben Elferink <ben.elferink@icloud.com>
2025-02-17 12:40:04 +0200

    fix: build error

--- src/components/header/header.stories.tsx
+++ src/components/header/header.stories.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import { Status } from '../status'
-import { Tooltip } from '../tooltip'
-import { Header, type HeaderProps } from '.'
-import { type StoryFn } from '@storybook/react'
-import { OdigosLogoText, SlackLogo, TerminalIcon } from '@odigos/ui-icons'
-import { PlatformSelect } from '../platform-select'
-import { NOTIFICATION_TYPE, PLATFORM_TYPE } from '@odigos/ui-utils'
 import Theme from '@odigos/ui-theme'
 import { IconButton } from '../icon-button'
-import { Text } from '../text'
+import { Header, type HeaderProps } from '.'
+import { type StoryFn } from '@storybook/react'
+import { NOTIFICATION_TYPE } from '@odigos/ui-utils'
+import { OdigosLogoText, SlackLogo, TerminalIcon } from '@odigos/ui-icons'
 
 export default {
   title: 'Components/Header',
@@ -19,12 +16,9 @@ export const Default: StoryFn<HeaderProps> = (props) => {
   return <Header {...props} />
 }
 
-const readonly = false
-
 Default.args = {
   left: [
     <OdigosLogoText size={100} />,
-    <PlatformSelect type={PLATFORM_TYPE.K8S} />,
     <Status
       status={NOTIFICATION_TYPE.SUCCESS}
       title='Connected'
@@ -34,11 +28,6 @@ Default.args = {
       withIcon
       withBackground
     />,
-    readonly && (
-      <Tooltip text="You're not allowed to create/update/delete in readonly mode">
-        <Status status={NOTIFICATION_TYPE.INFO} title='Read Only' size={14} family='primary' withIcon withBackground />
-      </Tooltip>
-    ),
   ],
 
   right: [
