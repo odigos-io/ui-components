20ba5bff3dd0b36e800c5a84a60aede88bb18f47
Ben Elferink <ben.elferink@icloud.com>
2025-02-25 11:31:23 +0200

    feat: enhance badge with new props (status/icon)

--- src/components/badge/badge.stories.tsx
+++ src/components/badge/badge.stories.tsx
@@ -1,18 +1,54 @@
 import React from 'react'
 import { Badge, type BadgeProps } from '.'
-import { type StoryFn } from '@storybook/react'
+import { NOTIFICATION_TYPE } from '@odigos/ui-utils'
+import type { StoryObj, StoryFn } from '@storybook/react'
 
 export default {
   title: 'Components/Badge',
   component: Badge,
 }
 
-export const Default: StoryFn<BadgeProps> = (props) => {
+// Create a master template for mapping props to render
+const Template: StoryFn<BadgeProps> = (props) => {
   return <Badge {...props} />
 }
 
+export const Default: StoryObj<BadgeProps> = Template.bind({})
+
 Default.args = {
   label: 69,
+}
+
+export const DefaultFilled: StoryObj<BadgeProps> = Template.bind({})
+
+DefaultFilled.args = {
+  label: 69,
   filled: true,
-  onClick: () => {},
+}
+
+export const StatusSuccess: StoryObj<BadgeProps> = Template.bind({})
+
+StatusSuccess.args = {
+  label: 69,
+  filled: true,
+  status: NOTIFICATION_TYPE.SUCCESS,
+  withIcon: true,
+}
+
+export const StatusError: StoryObj<BadgeProps> = Template.bind({})
+
+StatusError.args = {
+  label: 69,
+  filled: true,
+  status: NOTIFICATION_TYPE.ERROR,
+  withIcon: true,
+}
+
+export const StatusWarn: StoryObj<BadgeProps> = Template.bind({})
+
+StatusWarn.args = {
+  label: 69,
+  filled: true,
+  status: NOTIFICATION_TYPE.WARNING,
+  withIcon: true,
 }
--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -1,39 +1,59 @@
 import React, { type ReactNode, type FC } from 'react'
 import Theme from '@odigos/ui-theme'
 import styled from 'styled-components'
+import { getStatusIcon, NOTIFICATION_TYPE } from '@odigos/ui-utils'
 
 interface BadgeProps {
   label: string | number | ReactNode
   filled?: boolean
+  status?: NOTIFICATION_TYPE
+  withIcon?: boolean
   onClick?: () => void
 }
 
-const Styled = styled.span<{ $filled: BadgeProps['filled']; $clickable: boolean }>`
+const Styled = styled.span<{ $filled: BadgeProps['filled']; $status: BadgeProps['status']; $clickable: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   min-height: 16px;
   min-width: 16px;
   width: fit-content;
   padding: 4px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  gap: 2px;
+
   border-radius: 50px;
-  border: 1px solid ${({ theme, $filled }) => ($filled ? 'transparent' : theme.colors.border)};
-  background-color: ${({ theme, $filled }) => ($filled ? theme.colors.majestic_blue : 'transparent')};
-  color: ${({ theme, $filled }) => ($filled ? theme.text.white : theme.text.grey)};
+  border: 1px solid
+    ${({ theme, $filled, $status }) =>
+      !!$status ? theme.text[$status] + ($filled ? '' : Theme.opacity.hex['050']) : $filled ? 'transparent' : theme.colors.border};
+  background-color: ${({ theme, $filled, $status }) => ($filled ? (!!$status ? theme.colors[$status] : theme.colors.majestic_blue) : 'transparent')};
+  color: ${({ theme, $filled, $status }) =>
+    $filled && !!$status ? theme.text.secondary : $filled ? theme.text.white : !!$status ? theme.text[$status] : theme.text.grey};
   font-family: ${({ theme }) => theme.font_family.secondary};
   font-size: 12px;
   white-space: nowrap;
-  ${({ theme, $filled, $clickable }) =>
+
+  ${({ theme, $filled, $status, $clickable }) =>
     $clickable &&
     `&:hover {
       cursor: pointer;
-      background-color: ${$filled ? theme.colors.majestic_blue_soft : theme.colors.secondary + Theme.opacity.hex['020']};
+      background-color: ${
+        !!$status
+          ? theme.colors[$status] + Theme.opacity.hex['020']
+          : $filled
+          ? theme.colors.majestic_blue_soft
+          : theme.colors.secondary + Theme.opacity.hex['020']
+      };
     }`}
 `
 
-const Badge: FC<BadgeProps> = ({ label, filled, onClick }) => {
+const Badge: FC<BadgeProps> = ({ label, filled, status, withIcon, onClick }) => {
+  const theme = Theme.useTheme()
+  const Icon = !!status && withIcon ? getStatusIcon(status, theme) : null
+
   return (
-    <Styled $filled={filled} $clickable={!!onClick} onClick={onClick}>
+    <Styled $filled={filled} $status={status} $clickable={!!onClick} onClick={onClick}>
+      {!!Icon && <Icon />}
       {label}
     </Styled>
   )
